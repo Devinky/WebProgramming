225517d981123d3a9341b113afd5fb80801f1557
Devinky <devinky01@gmail.com>
2022-12-16 17:49:43 +0900

    221216 servlet

---
+++ WebProject/WebProject1216_2/src/org/project/dbconnect/DBConnect.java
@@ -0,0 +1,33 @@
+package org.project.dbconnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "system";
+		String psddword = "1234";
+		
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle Driver Connect Complete");
+			conn = DriverManager.getConnection(url, user, psddword);
+			System.out.println("Database Connect Complete");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Oracle Driver Not Found");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Database Connect Fail");
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+
+}
---
+++ WebProject/webProject1215/member1216.sql
@@ -0,0 +1,7 @@
+CREATE TABLE member1216
+(
+	userId varchar2(10) NOT NULL,
+	userpw varchar2(10) NOT NULL,
+	username varchar2(10) NOT NULL,
+	PRIMARY KEY (userId)
+);
